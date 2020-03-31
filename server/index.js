10f9dfceb42895fd1c3067c75cfc7ff05b4a723e
GrowthMates <ggrowthmates.com@gmail.com>
2020-03-31 22:41:47 +0500

    Again ups1

--- server/index.js
+++ server/index.js
@@ -253,7 +253,7 @@ setTimeout(()=>{
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build')); // serve the static react app
   app.get(/^\/(?!api).*/, (req, res) => { // don't serve api routes to react app
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
   console.log('Serving React App...');
 };

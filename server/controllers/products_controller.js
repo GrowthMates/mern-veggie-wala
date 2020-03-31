ff4d41aece637953e2e7f0ee4c9e0829302c9c26
GrowthMates <ggrowthmates.com@gmail.com>
2020-03-31 21:54:47 +0500

    Console

--- server/controllers/products_controller.js
+++ server/controllers/products_controller.js
@@ -6,7 +6,7 @@ module.exports = {
     
     readAllProducts(req, res){
         // var MySchema = mongoose.model('Product')
-        // console.log('Schema',MySchema)
+        console.log('ReadAllProducts called===')
         // Product.update({"name": "erer", "cartStock.cart" : "cart 1" } ,
         // {$inc : {"cartStock.$.stock" : 12} },
         // function(data) {console.log('hello world',data)});
@@ -21,7 +21,7 @@ module.exports = {
                 console.log('All products err--------',err);
                 return res.status(400).json({ productsNotFound: "No products Available" });
             }
-            // console.log('Products Find====',products.reverse())
+            console.log('Products Find====',products.reverse())
             res.status(200).send(products.reverse());
         })
 
